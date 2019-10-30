d634f67af144e0f15d275f6a7b460a06445a9baf
Christian Norrman <christian@faxity.se>
2019-10-30 22:55:14 +0100

    Tests finished

--- test/index.js
+++ test/index.js
@@ -19,24 +19,39 @@ async function assertUrl(target) {
   assert.equal(url, `${URL}${target}`);
 }
 
-async function assertH1(target) {
-  const $el = await browser.wait(until.elementLocated(By.css('h1')), 5000);
-  const text = await $el.getText();
-  assert.equal(text, target);
-}
-
 async function assertTitle(target) {
   const title = await browser.getTitle();
+
   assert.equal(title, `CakeStocks | ${target}`);
 }
 
-// Test go login
-// Test go register
-// Test change amount on home
-// Test add balance
-// Test buy one of something
-// Test sell one of something
-// Test 404 page?
+async function login() {
+  await clickLink('Login');
+  await sleep(500);
+  await assertTitle('Login');
+  await assertUrl('/login');
+
+  const $form = await browser.findElement(By.css('form'));
+  const $email = await $form.findElement(By.name('email'));
+  const $password = await $form.findElement(By.name('password'));
+
+  await $email.sendKeys('test@test.com');
+  await $password.sendKeys('Bananbr0d');
+  await $form.submit();
+
+  // Wait until form submitted fully
+  await browser.wait(until.elementLocated(By.css('h2')));
+  //await browser.wait(until.titleIs('CakeStocks | Hem'), 5000);
+  await assertTitle('Hem');
+  await assertUrl('/');
+}
+
+async function getText(css, parent = null) {
+  const $root = parent || browser;
+  const $el = await $root.findElement(By.css(css));
+
+  return $el.getText();
+}
 
 test.describe('Page', () => {
   // Hooks
@@ -52,85 +67,69 @@ test.describe('Page', () => {
   test.afterEach(() => browser.quit());
 
   // Test cases
+  test.it('Test go to login', async function () {
+    this.timeout(10000);
+    await login();
+  });
+
   test.it('Test go to register page', async function () {
     this.timeout(10000);
     await clickLink('Registrera');
-    await sleep(500);
-    await assertH1('Registrera konto');
     await assertTitle('Registrera');
     await assertUrl('/register');
-  });
-
-  test.it('Test go to login', async function () {
-    this.timeout(10000);
-    await clickLink('Login');
-    await sleep(500);
-    await assertH1('Login');
-    await assertTitle('Login');
-    await assertUrl('/login');
   });
 
   test.it('Test login and add balance', async function () {
     this.timeout(20000);
-    await clickLink('Login');
-    await sleep(500);
-    await assertH1('Login');
-    await assertTitle('Login');
-    await assertUrl('/login');
+    await login();
 
-    const $form = await browser.findElement(By.css('form'));
-    const $email = await $form.findElement(By.name('email'));
-    const $password = await $form.findElement(By.name('password'));
+    const oldBalance = await getText('.balance');
 
-    await $email.sendKeys('test@test.com');
-    await $password.sendKeys('Bananbr0d');
+    await clickLink('Betalmedel');
+
+    const $form = await browser.wait(until.elementLocated(By.css('form')), 5000);
+    const $amount = await $form.findElement(By.css('input[type=number]'));
+    await $amount.sendKeys(50);
     await $form.submit();
 
     // Wait until form has been submitted properly
-    await browser.wait(until.elementLocated(By.css('h2')), 5000);
+    await browser.wait(until.elementLocated(By.css('h2')));
     await assertTitle('Hem');
     await assertUrl('/');
+
+    const newBalance = await getText('.balance');
+    assert.ok(oldBalance != newBalance);
   });
 
-  /*test.it('Test login and buy item', async function () {
+  test.it('Test login and buy item', async function () {
+    this.timeout(20000);
+    await login();
 
+    const $card = await browser.findElement(By.css('.card'));
+    const oldAmount = await getText('.amount', $card);
+    const $btn = await $card.findElement(By.css('button.solid'));
+
+    await $btn.click();
+    await sleep(1000);
+
+    const newAmount = await getText('.amount', $card);
+
+    assert.ok(oldAmount != newAmount);
   });
 
   test.it('Test login and sell item', async function () {
+    this.timeout(20000);
+    await login();
 
-  });*/
+    const $card = await browser.findElement(By.css('.card'));
+    const oldAmount = await getText('.amount', $card);
+    const $btn = await $card.findElement(By.css('button'));
 
-  /* Test cases
-  test.it('Test go to register page', async function () {
-    this.timeout(10000);
-    await clickLink('Registrera');
-    await sleep(500);
-    await assertH1('Registrera konto');
-    await assertTitle('Registrera');
-    await assertUrl('/register');
+    await $btn.click();
+    await sleep(1000);
+
+    const newAmount = await getText('.amount', $card);
+
+    assert.ok(oldAmount != newAmount);
   });
-
-  test.it('Test go to login page', async function () {
-    this.timeout(10000);
-    await clickLink('Login');
-    await sleep(500);
-    await assertH1('Login');
-    await assertTitle('Login');
-    await assertUrl('/login');
-  });
-
-  test.it('Test go to first report', async function () {
-    this.timeout(10000);
-    await clickLink('Rapporter');
-    await assertH1('Rapporter');
-    await assertTitle('Rapporter');
-    await assertUrl('/reports');
-
-    // Click report for week 1
-    await clickLink('Rapport vecka 1');
-    await sleep(500);
-    await assertH1('Rapport vecka 1');
-    await assertTitle('Rapport vecka 1');
-    await assertUrl('/reports/week/1');
-  });*/
 });
